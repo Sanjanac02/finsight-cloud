c8841b276f3ebe5efae1493bb66d8c3c94ab72e6
Sanjana Changwal <sanjanachangwal@gmail.com>
2026-09-09 22:13:39 +0530

    Allow public actuator health check

--- src/main/java/com/sanjana/finsightcloud/config/SecurityConfig.java
+++ src/main/java/com/sanjana/finsightcloud/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .anyRequest().authenticated()
                 );
 
